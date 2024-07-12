155497af31c4f05bc555733d5e99b223120ba1f6
eric <ericyuanovo@gmail.com>
2024-07-12 16:32:37 +0800

    update siteMetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: '',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog', // TODO
+  siteUrl: 'https://www.twotwoba.site',
+  siteRepo: 'https://github.com/twotwoba/blog', // TODO
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   // mastodon: 'https://mastodon.social/@mastodonuser',
