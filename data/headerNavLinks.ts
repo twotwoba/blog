ae5765761c1bb5136e70f9b145d7041195a8100e
eric.yuan <eric.yuan@yunlsp.com>
2025-06-11 10:26:38 +0800

    refactor files

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
-    { href: '/', title: 'About' },
+    { href: '/', title: 'Me' },
     { href: '/blog', title: 'Blog' }
+    // 下面的功能页保留，暂不需要，my blog，the simple the better.
     // { href: '/tags', title: 'Tags' }
     // { href: '/projects', title: 'Projects' }
 ]
