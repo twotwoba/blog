ddb647f39006dcc7b73e42e0991310ec2de61376
eric.yuan <eric.yuan@yunlsp.com>
2025-06-11 17:33:54 +0800

    update some article

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,7 @@
 const headerNavLinks = [
     { href: '/', title: 'Me' },
     { href: '/blog', title: 'Blog' }
-    // 下面的功能页保留，暂不需要，my blog，the simple the better.
-    // { href: '/tags', title: 'Tags' }
+    // TODO open soon~
     // { href: '/projects', title: 'Projects' }
 ]
 
